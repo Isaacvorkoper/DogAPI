193a69452ac393411a48a32c253fa560d64dfb8d
Isaac Lennart Vorköper <isaa0052@edu.sde.dk>
2020-09-21 14:57:48 +0200

    Finished API fetching.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,13 +28,12 @@ namespace DogAPI
         public MainWindow()
         {
             InitializeComponent();
-            raceBox.Items.Add("finnish-lapphund");
+            raceBox.Items.Add("germanshepherd");
+            raceBox.Items.Add("chow");
 
-            
-
-           
         }
 
+        //Sletter start teksten når den bliver trykket på
         private void raceSoeg_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
         {
             TextBox txtBox = sender as TextBox;
@@ -37,20 +41,51 @@ namespace DogAPI
                 txtBox.Text = string.Empty;
         }
 
+        //Søger efter teksten indtastet i search boxen 
         private void Soeg_Click(object sender, RoutedEventArgs e)
         {
             string race = raceSoeg.Text;
-            string apistring = "https/fesfesfe/breed/" + raceSoeg.Text + "";
+ 
+            string apistring = "https://dog.ceo/api/breed/" + raceSoeg.Text  + "/images/random";
+
+            try
+            {
+                doggoPic.Source = new BitmapImage(new Uri(hentBillede(apistring)));
+            }
+            catch (Exception)
+            {
+            }
         }
 
+        //Henter billede af racen valgt på listen
         private void raceBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             string apistring = "https://dog.ceo/api/breed/"+ raceBox.SelectedItem + "/images/random";
+
+            try
+            {
+                doggoPic.Source = new BitmapImage(new Uri(hentBillede(apistring)));
+            }
+            catch (Exception)
+            {
+            }
+
         }
 
-        private void hentBillede(string API)
+        private string hentBillede(string API)
         {
-
+            try
+            {
+                var Json = new WebClient().DownloadString(API);
+                string[] temp = Json.Split('"');
+                temp[3] = temp[3].Replace(@"\", "");
+                return temp[3];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + " Dog may not exist");
+            }
+            return null;
         }
     }
 }
